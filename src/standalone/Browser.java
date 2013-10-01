679f4fc848ca8a81d54110255f9b4dc599aaf07a
Alejandro Huertas <alejandro.huertas@globant.com>
2013-10-01 13:10:38 -0300

    Leap listener integrated with folder browser navigate through files

--- src/standalone/Browser.java
+++ src/standalone/Browser.java
@@ -2,13 +2,24 @@ package standalone;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Gesture;
+import com.leapmotion.leap.GestureList;
+import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.SwipeGesture;
+
 public class Browser extends JFrame{
+
+    static Browser browser;
     
     JList foldersList;
     JPanel panel2 ;
@@ -58,13 +69,84 @@ public class Browser extends JFrame{
         }
     }
     
+    class LeapListenerController extends Listener {
+        
+        public void onInit(Controller controller) {
+            System.out.println("Initialized");
+        }
+        
+        public void onConnect(Controller controller) {
+            System.out.println("Connected");
+            controller.enableGesture(Gesture.Type.TYPE_SWIPE);
+            controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
+        }
+        
+        public void onDisconnect(Controller controller) {
+            //Note: not dispatched when running in a debugger.
+            System.out.println("Disconnected");
+        }
+        
+        public void onExit(Controller controller) {
+            System.out.println("Exited");
+        }
+        
+        
+        public void onFrame(Controller controller) {
+            // Get the most recent frame and report some basic information
+            Frame frame = controller.frame();
+            
+            GestureList gestures = frame.gestures();
+            for (int i = 0; i < gestures.count(); i++) {
+                Gesture gesture = gestures.get(i);
+                if (gesture.duration()>=10000 && gesture.state().toString().equals("STATE_STOP")){
+                    System.out.println("Gesture type --> "+gesture.type()+ ", Duration -->" +gesture.duration());
+                    
+                    switch (gesture.type()) {
+                    
+                    case TYPE_SWIPE:
+                        SwipeGesture swipe = new SwipeGesture(gesture);
+                        // if it is left to right swipe 
+                        if (swipe.direction().get(0)>0 && swipe.direction().get(1)>0){
+                            browser.moveToNextFile();
+                        }
+                        // if it is right to left swipe
+                        else if (swipe.direction().get(0)<0 && swipe.direction().get(1)<0){
+                            browser.moveToPreviousFile();
+                        }
+                    break;
+                        
+                    default:
+                        System.out.println("Unknown gesture type.");
+                    break;
+                    }
+                }
+            }
+            
+            if (frame.hands().isEmpty() && gestures.isEmpty()) {
+            }
+        }
+    }
+    
     public static void main (String... args){
-        Browser browser = new Browser();
+        browser = new Browser();
         
         browser.setVisible(true);
         
-        browser.moveToNextFile();
-        browser.moveToNextFile();
-//        browser.moveToPreviousFile();
+        LeapListenerController leapListener = browser.new LeapListenerController();
+        Controller controller = new Controller();
+        
+        // Have the sample listener receive events from the controller
+        controller.addListener(leapListener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(leapListener);
     }
 }
